59c1ec5d5d34970506755741d85957d3e6192fa7
Juan Díaz <3674588+JuanoD@users.noreply.github.com>
2025-03-19 16:19:15 -0500

    helix lazygit

---
+++ .config/helix/lzg.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+# TODO: Find a way to open files from here
+lazygit
+
+# if [[ -n "$paths" ]]; then
+#     zellij action toggle-floating-panes
+#     zellij action write 27 # send <Escape> key
+#     zellij action write-chars ":$1 $paths"
+#     zellij action write 13 # send <Enter> key
+# else
+    zellij action toggle-floating-panes
+# fi
